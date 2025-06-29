a6e69ff1b2a8629ee201597a55f34505d43efd5e
saparyp <111504965+saparyp@users.noreply.github.com>
2025-06-30 02:22:53 +0300

    Add files via upload

--- test.sh
+++ test.sh
@@ -1,3 +1,26 @@
+#!/bin/sh
+
+set -e
+
+buildFrontend() {
+    DOCKER_BUILDKIT=1 docker build -f frontend.Dockerfile frontend/ --tag frontend:v1.0
+}
+
+duildBackend() {
+    ./backend/gradlew clean build -p backend
+    DOCKER_BUILDKIT=1 docker build -f backend.Dockerfile backend/ --tag backend:v1.0
+}
+
+createNetworks() {
+    docker network create --driver=bridge network-backend
+
+    docker network create --driver=brige network-frontend
+}
+
+createVolume() {
+    docker volume create postgres-data
+}
+
 runPostgres() {
     docker run -d \
         --name postgres \
@@ -28,3 +51,40 @@ runFrontend() {
         --network=network-frontend \
         frontend:v1.0
 }
+checkResult() {
+  sleep 10
+  http_response=$(
+    docker exec \
+      frontend \
+      curl -s -o response.txt -w "%{http_code}" http://backend-service:8080/backend/api/v1/public/items
+  )
+
+  if [ "$http_response" != "200" ]; then
+    echo "Check failed"
+    exit 1
+  fi
+}
+
+echo "=== Build backend backend:v1.0 ==="
+buildBackend
+
+echo "=== Build frontend frontend:v1.0 ==="
+buildFrontend
+
+echo "=== Create networks between backend <-> postgres and backend <-> frontend ==="
+createNetworks
+
+echo "=== Create persistence volume for postgres ==="
+createVolume
+
+echo "== Run Postgres ==="
+runPostgres
+
+echo "=== Run backend backend:v1.0 ==="
+runBackend
+
+echo "=== Run frontend frontend:v1.0L ==="
+runFrontend
+
+echo "=== Run check ==="
+checkResult
